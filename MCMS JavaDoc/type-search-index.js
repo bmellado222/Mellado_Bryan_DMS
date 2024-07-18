b486c63a4e7e1fed4b610183f0dced8f4b05c041
bmellado222 <bmellado222@gmail.com>
2024-07-18 19:00:15 -0400

    Add files via upload

---
+++ MCMS JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Artist"},{"p":"<Unnamed>","l":"Catalog"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Song"},{"p":"<Unnamed>","l":"TestClassCatalog"}];updateSearchResults();
